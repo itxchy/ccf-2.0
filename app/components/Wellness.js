74fa7e16b171c2ce4619d5b16088017312ce4da4
Matt Trifilo <matt.trifilo@gmail.com>
2016-04-11 17:23:58 -0500

    style the content for the Wellness view

--- app/components/Wellness.js
+++ app/components/Wellness.js
@@ -6,20 +6,27 @@ let Wellness = React.createClass({
 		return (
 			<div className="container wellness">
 				<div className="textbox">
-					<h1>Cory Martin's Philosophy</h1>
+					<h1 className="wellness-title">Cory Martin's Philosophy</h1>
 					<p>
 						Cory likes to take the role as a wellness coach to teach and 
 						build healthy habits for his clients that are sustainable and 
-						can be carried on for the rest of their lives. With the vast 
-						amount of information available to us about fitness and nutrition, 
+						can be carried on for the rest of their lives. 
+					</p><br/>
+					<p>
+						With the vast amount of information available to us about fitness and nutrition, 
 						he helps his clients navigate those waters and determine 
 						what information is real and what is pseudoscience. All of 
-						his information will be backed by real science and proven studies. 
+						his information will be backed by real science and proven studies.
+					</p><br/>
+					<p>	 
 						Whether your goals are weight loss, strength, muscle building, 
 						or just being more active, he approaches it from the same philosophy. 
 						By striving to make the body as healthy as possible with wellness 
 						in mind, you will see the most changes internally (How you feel.) 
-						and externally ( How you look.) Under his guidance, he gives the 
+						and externally (How you look). 
+        			</p>
+        			<p><br/>
+						Under his guidance, he gives the 
 						tools to his clients, so they can take charge of their own wellness 
 						journey.
 					</p>
